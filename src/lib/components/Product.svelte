4d129bc2cd719c87f6327b4fc0dbe6c3207b76c4
Enny Hellsén <enny.hellsen.j@gmail.com>
2024-09-24 12:35:25 +0200

    refactor: move product item into its own component

---
+++ src/lib/components/Product.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import "../../app.css"
+
+  export let product: any
+
+	let clickedItem = ''
+  const toggleModal = (item : any) => {
+    clickedItem = clickedItem === item.id ? '' : item.id
+  }
+
+</script>
+
+<button on:click={() => toggleModal(product)} class="product">
+
+  <div class="image-container">
+    <img class="image" src={product.images[0]} alt={product.title}/>
+    <span 
+      class="availability" 
+      style:background-color={product.availabilityStatus === 'In Stock' ? 'green' : 'red'}
+    > 
+    {product.availabilityStatus}</span>
+  </div>
+
+  <h2>{product.title}</h2>
+  <p> {product.price}</p>
+
+  <div class={clickedItem === product.id ? 'modal-visible' : 'modal-hidden'}>
+    <p> {product.description} </p>
+    <p class="italic"> Height: {product.dimensions.height} cm</p>
+    <p class="italic"> Width: {product.dimensions.width} cm</p>
+    <p class="italic"> Depth: {product.dimensions.depth} cm</p>
+
+    <p> <span class="font-bold"> Rating: </span> {product.rating}</p>
+  </div>
+</button>
+
+<style>
+
+.product {
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    grid-gap: 16px;
+    cursor: pointer;
+    text-align: left;
+  }
+
+.image-container {
+    background-color: #f1f0f0;
+    padding: 30px 20px;
+    grid-column: 1 / 3;
+    border-radius: 7px;
+    position: relative;
+  }
+
+  .image-container:hover {
+    background-color: #e0dede;
+  }
+
+  .image {
+    width: 100%;
+  }
+
+  .availability {
+    color: white;
+    right: 0;
+    bottom: 30px;
+    padding: 5px;
+    position: absolute;
+    border-radius: 5px 0 0 5px;
+  }
+
+  .modal-visible {
+    display: block;
+    position: absolute;
+    background-color: bisque;
+    width: 300px;
+  }
+
+  .modal-hidden {
+    display: none;
+  }
+	
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import "../app.css"
+  import Product from '../lib/components/Product.svelte'
 
   interface Data {
     products: {
@@ -25,11 +26,6 @@
   const products = data.products.products
   console.log(products)
 
-  let clickedItem = ''
-  const toggleModal = (item : any) => {
-    clickedItem = clickedItem === item.id ? '' : item.id
-  }
-
 </script>
 
 <style>
@@ -46,50 +42,6 @@
     margin: 0 20px;
     position: relative;
   }
-
-  .product {
-    display: grid;
-    grid-template-columns: 1fr max-content;
-    grid-gap: 16px;
-    cursor: pointer;
-    text-align: left;
-  }
-
-  .product:hover .image-container {
-    background-color: #e0dede;
-  }
-
-  .image-container {
-    background-color: #f1f0f0;
-    padding: 30px 20px;
-    grid-column: 1 / 3;
-    border-radius: 7px;
-    position: relative;
-  }
-
-  .image {
-    width: 100%;
-  }
-
-  .availability {
-    color: white;
-    right: 0;
-    bottom: 30px;
-    padding: 5px;
-    position: absolute;
-    border-radius: 5px 0 0 5px;
-  }
-
-  .modal-visible {
-    display: block;
-    position: absolute;
-    background-color: bisque;
-    width: 300px;
-  }
-
-  .modal-hidden {
-    display: none;
-  }
 </style>
 
 <main class="page-layout">
@@ -97,29 +49,7 @@
 
 <div class="product-container">
   {#each products as product}
-    <button on:click={() => toggleModal(product)} class="product">
-
-      <div class="image-container">
-        <img class="image" src={product.images[0]} alt={product.title}/>
-        <span 
-          class="availability" 
-          style:background-color={product.availabilityStatus === 'In Stock' ? 'green' : 'red'}
-        > 
-        {product.availabilityStatus}</span>
-      </div>
-
-      <h2>{product.title}</h2>
-      <p> {product.price}</p>
-
-      <div class={clickedItem === product.id ? 'modal-visible' : 'modal-hidden'}>
-        <p> {product.description} </p>
-        <p class="italic"> Height: {product.dimensions.height} cm</p>
-        <p class="italic"> Width: {product.dimensions.width} cm</p>
-        <p class="italic"> Depth: {product.dimensions.depth} cm</p>
-
-        <p> <span class="font-bold"> Rating: </span> {product.rating}</p>
-      </div>
-    </button>
+    <Product {product}/>
   {/each}
 </div>
 
